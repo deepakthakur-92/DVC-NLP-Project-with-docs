578e0ed42df762aa781158e6c1c51585641215e3
Deepak Thakur <deepak.thakur@3pillarglobal.com>
2023-03-22 14:41:32 +0530

    stage 04 scripting complete

---
+++ research_trail_dir/prc_explanation.py
@@ -0,0 +1,9 @@
+
+
+precision, recall, prc_threshold = [0.1,0.2,0.4,0.9,0.99,0.2,0.66], [0.1,0.2,0.4,0.9,0.99,0.2,0.66],[0.1,0.2,0.4,0.9,0.99,0.2,0.66]
+
+
+n_th = 4
+#print(list(zip(precision, recall, prc_threshold))[::2])
+print(list(zip(precision, recall, prc_threshold))[::3])
+print(list(zip(precision, recall, prc_threshold))[::n_th])
